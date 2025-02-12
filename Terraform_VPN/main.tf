beb3978096bf68640301c3b27dc29e7b1607b612
RobinThiriet <thiriet.robin@live.fr>
2025-02-12 13:16:30 +0100

    valide

--- Terraform_VPN/main.tf
+++ Terraform_VPN/main.tf
@@ -210,7 +210,7 @@ resource "aws_security_group" "cozycloud_sg" {
 }
 
 # Instance EC2 Bastion
-resource "aws_instance" "bastion_ami" {
+resource "aws_instance" "bastion" {
   ami           = var.bastion_ami
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.public_subnet.id
@@ -224,7 +224,7 @@ resource "aws_instance" "bastion_ami" {
 }
 
 # Instance EC2 Cozycloud
-resource "aws_instance" "cozycloud_ami" {
+resource "aws_instance" "cozycloud" {
   ami           = var.cozycloud_ami
   instance_type = "t2.medium"
   subnet_id     = aws_subnet.private_subnet.id
