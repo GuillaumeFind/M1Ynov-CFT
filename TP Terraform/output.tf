9ae1db3c332db36a807f10c95c5e753c4a5cfa12
RobinThiriet <thiriet.robin@live.fr>
2025-02-13 08:33:07 +0100

    Delete TP Terraform directory

--- TP Terraform/output.tf
+++
@@ -1,9 +0,0 @@
-output "load_balancer_dns" {
-  description = "Nom DNS du Load Balancer"
-  value       = aws_lb.RTT-LoadBalancer.dns_name
-}
-
-output "autoscaling_group_arn" {
-  description = "ARN du groupe Auto Scaling"
-  value       = aws_autoscaling_group.RTT-AutoScaling.arn
-}
